f59f796f4d12496a55d4b095d3cb6fb85521e58b
David_dev <92552378+Davidtricolor2021@users.noreply.github.com>
2025-01-21 20:25:48 -0300

    Correções em login, listar despesas e estilização do sistema

--- gastos/urls.py
+++ gastos/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login_view, name='login'),
     path('', views.listar_despesas, name='listar_despesas'), # Página inicial
     path('adicionar/', views.adicionar_despesa, name='adicionar_despesa'), # Adicionar despesa
 ]
--- gastos/views.py
+++ gastos/views.py
@@ -2,12 +2,54 @@ from django.shortcuts import render, redirect
 from .models import Despesa, Categoria
 from django.contrib.auth.decorators import login_required
 from datetime import datetime, timedelta
+from collections import defaultdict
+from django.db.models.functions import TruncMonth
+from django.db.models import Sum
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 import calendar
+
+def login_view(request):
+    if request.user.is_authenticated:
+        # Se já estiver logado, redireciona para a página principal
+        return redirect('listar_despesas')  # ou qualquer outra página desejada
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            # Faz o login e redireciona para a página principal
+            user = form.get_user()
+            login(request, user)
+            return redirect('listar_despesas')  # ou qualquer outra página desejada
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'gastos/login.html', {'form': form})
 
 @login_required
 def listar_despesas(request):
-    despesas = Despesa.objects.filter(usuario=request.user)
-    return render(request, 'gastos/listar_despesas.html', {'despesas': despesas})
+    # Agrupar as despesas por mês/ano
+    despesas_por_mes = defaultdict(list)
+    despesas = Despesa.objects.filter(usuario=request.user).annotate(mes=TruncMonth('data_pgto'))
+
+    # Organizar as despesas por mês
+    for despesa in despesas:
+        key = despesa.data_pgto.strftime('%Y-%m')  # Exemplo: "2025-01"
+        despesas_por_mes[key].append(despesa)
+
+    # Ordenar os meses de forma cronológica
+    despesas_por_mes = dict(sorted(despesas_por_mes.items()))
+
+    # Gerar os nomes dos meses (Exemplo: "Jan/2025")
+    meses_formatados = {
+        key: despesa[0].data_pgto.strftime('%b/%Y')  # Exemplo: "Jan/2025"
+        for key, despesa in despesas_por_mes.items()
+    }
+
+    return render(request, 'gastos/listar_despesas.html', {
+        'despesas_por_mes': despesas_por_mes,
+        'meses_formatados': meses_formatados,
+    })
 
 @login_required
 def adicionar_despesa(request):
